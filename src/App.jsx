e0816fb1ac2ea6f99a7ab6346577bbb20cb7b7c3
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-05-04 09:07:33 +0200

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,49 +2,50 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "swiper/css";
 
-// import HeaderNavbar from "./component/HeaderNavbar";
-// import FilmsCarousel from "./component/FilmsCarousel";
-// import { Container, Row, Col } from "react-bootstrap";
-// import MyFooter from "./component/MyFooter";
-import Profile from "./component/Profile";
+import HeaderNavbar from "./component/HeaderNavbar";
+import FilmsCarousel from "./component/FilmsCarousel";
+import { Container, Row, Col } from "react-bootstrap";
+import MyFooter from "./component/MyFooter";
+// import Profile from "./component/Profile";
 
 function App() {
   return (
-    // <Container fluid>
-    //   <header>
-    //     <HeaderNavbar></HeaderNavbar>
-    //   </header>
-    //   <main>
-    //     <Row className="my-3">
-    //       <Col>
-    //         <h1 className=" text-light">MADAGASCAR</h1>
-    //       </Col>
-    //       <Col>
-    //         <FilmsCarousel movie="madagascar"></FilmsCarousel>
-    //       </Col>
-    //     </Row>
-    //     <Row className="my-3">
-    //       <Col>
-    //         <h1 className=" text-light">AVENGERS</h1>
-    //       </Col>
-    //       <Col>
-    //         <FilmsCarousel movie="avengers"></FilmsCarousel>
-    //       </Col>
-    //     </Row>
-    //     <Row className="my-3">
-    //       <Col>
-    //         <h1 className=" text-light">BATMAN</h1>
-    //       </Col>
-    //       <Col>
-    //         <FilmsCarousel movie="batman"></FilmsCarousel>
-    //       </Col>
-    //     </Row>
-    //   </main>
-    //   <footer className="d-flex justify-content-center">
-    //     <MyFooter></MyFooter>
-    //   </footer>
-    // </Container>
-    <Profile></Profile>
+    <Container fluid>
+      <header>
+        <HeaderNavbar></HeaderNavbar>
+      </header>
+      <main>
+        <Row className="my-3">
+          <Col>
+            <h1 className=" text-light">MADAGASCAR</h1>
+          </Col>
+          <Col>
+            <FilmsCarousel movie="madagascar"></FilmsCarousel>
+          </Col>
+        </Row>
+        <Row className="my-3">
+          <Col>
+            <h1 className=" text-light">AVENGERS</h1>
+          </Col>
+          <Col>
+            <FilmsCarousel movie="avengers"></FilmsCarousel>
+          </Col>
+        </Row>
+        <Row className="my-3">
+          <Col>
+            <h1 className=" text-light">BATMAN</h1>
+          </Col>
+          <Col>
+            <FilmsCarousel movie="batman"></FilmsCarousel>
+          </Col>
+        </Row>
+      </main>
+      <footer className="d-flex justify-content-center">
+        <MyFooter></MyFooter>
+      </footer>
+    </Container>
+
+    // <Profile></Profile> PAGINA DEL PROFILO
   );
 }
 
