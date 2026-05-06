ffea3db4d7aed41ebcc9390bd54e990addf11a86
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-05-06 17:48:27 +0200

    fixing bugs

--- src/component/MovieDetails.jsx
+++ src/component/MovieDetails.jsx
@@ -1,19 +1,21 @@
-import { useParams, useState } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Card } from "react-bootstrap";
+import { useEffect, useState } from "react";
 
 const MovieDetails = () => {
-  const [movies, setMovies] = useState({});
-  const param = useParams();
+  const [movies, setMovies] = useState(null);
+  const param = useParams().movieId;
+  console.log(param);
 
   const getMovies = async () => {
     try {
       const response = await fetch(
-        `http://www.omdbapi.com/?t=${param.movieId}&apikey=b7e1565c`,
+        `http://www.omdbapi.com/?apikey=b7e1565ci=` + param,
       );
       if (response.ok) {
         const data = await response.json();
 
-        setMovies = data.Search;
+        setMovies(data);
       } else {
         throw new Error("ERRORE NEL JSON", response.status);
       }
@@ -22,16 +24,28 @@ const MovieDetails = () => {
     }
   };
 
+  //   const foundMovie = movies.find((film) => {
+  //     return film.id.toString() === param;
+  //   });
+
+  useEffect(() => {
+    const foundMovie = movies.find((film) => {
+      return movies.imdbID.toString() === param;
+    }); // foundPasta è UNO degli oggetti del JSON, quello in cui ha trovato un match tra il proprio id
+    setMovies(foundMovie);
+  }, []);
+
+  //   useEffect(() => {
+  //     getMovies();
+  //   }, [param]);
+
   return (
     <>
       <Card>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src={movies.Poster} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
+          <Card.Title>{movies.Title}</Card.Title>
+          <Card.Text>{movies.Title}</Card.Text>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
